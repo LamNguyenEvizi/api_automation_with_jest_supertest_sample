9e0510ebf16b2466dd0f3b1136d5113249e0218b
LamNguyenEvizi <114992278+LamNguyenEvizi@users.noreply.github.com>
2024-08-29 13:57:17 +0700

    Add files via upload

---
+++ __tests__/sanity.spec.ts
@@ -0,0 +1,9 @@
+import request from "supertest";
+
+const baseUrl = process.env.ENDPOINT;
+
+describe("Sanity test", () => {
+    it("Sanity test", async () => {
+        console.log('Sanity test: Passed');
+    });
+});
